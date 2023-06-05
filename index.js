c2537b5f5a8b93f5c230c4a5b1c7047e39a8aa61
LinCoding2910 <tanguanlin908@gmail.com>
2023-06-05 16:59:59 +0800

    game's GUI done

--- index.js
+++ index.js
@@ -6,37 +6,90 @@ function getComputerChoice () {
     return randomChoice    
 }
 
-console.log(getComputerChoice())
+let playerScore = 0;
+let computerScore = 0;
+console.log(playerScore)
 
-// let playerSelectio = "paper"
-// let computerSelectio = getComputerChoice()
-// console.log(playerSelectio)
-// console.log(computerSelectio)
-
-function playRound (playerSelection,computerSelection) {
-    let x = playerSelection
-    let y = computerSelection
-    if (x==="rock" && y==="scissor") {
-        return ("You win! Rock beats scissor.")
-    } else if (x==="scissor" && y==="paper") {
-        return ("You win! Scissors beats paper.")
-    } else if (x==="paper" && y==="rock") {
-        return ("You win! Paper beats rock.")
-    } else if (x==="scissor" && y==="rock") {
-        return ("You lose! Rock beats scissor.") 
-    } else if (x==="paper" && y==="scissor") {
-        return ("You lose! Scissor beats paper.")
-    } else if (x==="rock" && y==="paper") {
-        return ("You lose! Paper beats rock.")
-    } else if (x==="rock" && y==="rock") {
-        return ("It's a tie.")
-    } else if (x==="scissor" && y==="scissor") {
-        return ("It's a tie.")
-    } else if (x==="paper" && y==="paper") {
-        return ("It's a tie.")
+function playRound (selection) {
+    let playerSelection = selection
+    let computerSelection = getComputerChoice().toString()
+    if (playerScore === 5 || computerScore === 5) {
+        return;
+    };
+    if (playerSelection==="rock" && computerSelection==="scissor") {
+        playerScore++;
+        playerScoreDisplay.textContent = playerScore;
+        result.textContent = "You win! Rock beats scissor.";
+        return playerScore;
+    } else if (playerSelection==="scissor" && computerSelection==="paper") {
+        playerScore++;
+        playerScoreDisplay.textContent = playerScore;
+        result.textContent = "You win! Scissors beats paper.";
+    } else if (playerSelection==="paper" && computerSelection==="rock") {
+        playerScore++;
+        playerScoreDisplay.textContent = playerScore;
+        result.textContent = "You win! Paper beats rock.";
+    } else if (playerSelection==="scissor" && computerSelection==="rock") {
+        computerScore++;
+        computerScoreDisplay.textContent = computerScore;
+        result.textContent = "You lose! Rock beats scissor." ;
+    } else if (playerSelection==="paper" && computerSelection==="scissor") {
+        computerScore++;
+        computerScoreDisplay.textContent = computerScore;
+        result.textContent = "You lose! Scissor beats paper.";
+    } else if (playerSelection==="rock" && computerSelection==="paper") {
+        computerScore++;
+        computerScoreDisplay.textContent = computerScore;
+        result.textContent = "You lose! Paper beats rock.";
     } else {
-        return ("You've entered something wrong")
-    }
+        result.textContent = "It's a tie."
+    } 
+    if (playerScore === 5) {
+        result.textContent = "Congrats! You've won!"; 
+    } else if (computerScore === 5) {
+        result.textContent = "Unlucky! You've lost!";
+    } 
 }
 
-console.log (playRound("rock","paper"))
+const body = document.querySelector('body');
+const buttonRock = document.createElement('button');
+const buttonPaper = document.createElement('button');
+const buttonScissor = document.createElement('button');
+const result = document.createElement('div');
+const computerScoreDisplay = document.createElement('div');
+const playerScoreDisplay = document.createElement('div');
+
+body.append(result,buttonRock,buttonPaper,buttonScissor,playerScoreDisplay,computerScoreDisplay);
+
+playerScoreDisplay.textContent = playerScore;
+computerScoreDisplay.textContent = computerScore;
+buttonRock.textContent = "ROCK";
+buttonPaper.textContent = "PAPER";
+buttonScissor.textContent = "SCISSOR";
+
+buttonRock.addEventListener('click',() => {
+    playRound("rock");
+});
+buttonPaper.addEventListener('click',() => {
+    playRound("paper");
+});
+buttonScissor.addEventListener('click',() => {
+    playRound("scissor");
+});
+
+let game = () => {
+    alert('SUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII!!!!')
+    for (let gameWin = 0;gameWin === 1;) {
+        if (playerScore === 5) {
+            result.textContent = "Congrats! You've won!";
+            gameWin = 1; 
+        };   
+        if (computerScore === 5) {
+            result.textContent = "Unlucky! You've lost!";
+            gameWin = 1; 
+        };
+    };
+};
+game()
+
+alert('SUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII!!!!')
